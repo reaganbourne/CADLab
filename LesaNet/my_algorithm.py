e0e92ec5ddc3a8164a582bd9a0a632ff798f4997
Reagan Bourne <reaganebourne@gmail.com>
2023-06-07 21:38:20 -0400

    I added a few comments!

--- LesaNet/my_algorithm.py
+++ LesaNet/my_algorithm.py
@@ -14,32 +14,34 @@ from config import config, default
 
 
 def select_triplets_multilabel(emb, target):
-    target = target.detach().cpu().numpy()
-    num_smp = len(target)
-    inters = np.matmul(target, target.T)
-    iou = 1 - squareform(pdist(target, 'jaccard'))
-    sim = inters * iou + np.diag(np.nan*np.ones((num_smp,)))
+    target = target.detach().cpu().numpy()                          #converts target tensor to numpy array and detach it from CPU
+    num_smp = len(target)                                           #gets the number of samples
+    inters = np.matmul(target, target.T)                            #calculates the intersection between target samples
+    iou = 1 - squareform(pdist(target, 'jaccard'))                  #calculates the jaccard distance between target labels
+    sim = inters * iou + np.diag(np.nan*np.ones((num_smp,)))        #calculates the similarity matrix by combining the intersection and jaccard distance
     # sim = inters + np.diag(np.nan*np.ones((num_smp,)))
 
-    triplets = []
-    for p in range(config.TRAIN.NUM_TRIPLET):
-        a = np.random.choice(num_smp)
-        p_candidates = np.where(sim[a] >= config.TRAIN.SIMILAR_LABEL_THRESHOLD)[0]
+    triplets = []                                                   #initializes a list to store the triplets
+    for p in range(config.TRAIN.NUM_TRIPLET):                       #generates the triplets
+        a = np.random.choice(num_smp)                               #sets the anchor randomly
+        p_candidates = np.where(sim[a] >= config.TRAIN.SIMILAR_LABEL_THRESHOLD)[0]              #finds a positive candidate thats >=threshold
         # p = np.random.choice(np.setdiff1d(range(num_smp), [a]))
-        if len(p_candidates) == 0:
+        if len(p_candidates) == 0:                                  #if there are no positive candidates, then find one with the highest similarity
             p = np.nanargmax(sim[a])
         else:
-            p = np.random.choice(p_candidates)
-        if sim[a, p] - np.nanmin(sim[a]) <= config.TRAIN.DISSIMILAR_LABEL_THRESHOLD:
+            p = np.random.choice(p_candidates)          
+        if sim[a, p] - np.nanmin(sim[a]) <= config.TRAIN.DISSIMILAR_LABEL_THRESHOLD: #check if similarity between anchor and positive sample <= threshold
             n = np.nanargmin(sim[a])
             # if sim[a,p] == sim[a,n]:
             #     continue
         else:
-            n_candidates = np.where(sim[a] < sim[a, p] - config.TRAIN.DISSIMILAR_LABEL_THRESHOLD)[0]
+            n_candidates = np.where(sim[a] < sim[a, p] - config.TRAIN.DISSIMILAR_LABEL_THRESHOLD)[0]        #find negative candidates based on dissimalarity threshold
             n = np.random.choice(n_candidates)
-        triplets.append([a, p, n])
+        triplets.append([a, p, n])                                  #append the triplet to the triplets list
 
-    A = emb[[triplet[0] for triplet in triplets]]
+    A = emb[[triplet[0] for triplet in triplets]]                   #selects the corresponding embeddings
     P = emb[[triplet[1] for triplet in triplets]]
     N = emb[[triplet[2] for triplet in triplets]]
-    return A, P, N
+    return A, P, N                                                  #returns the embeddings
+
+#This code selects embeddings corresponding to the anchor, positive and negative samples, and returns them as triplets
--- LesaNet/my_loss.py
+++ LesaNet/my_loss.py
@@ -17,17 +17,17 @@ from utils import unique
 
 
 class WeightedCeLoss(nn.Module):
-    def __init__(self, pos_weight, neg_weight):
+    def __init__(self, pos_weight, neg_weight):                                                     #initializes the pos and neg weight, and converts weights to tensors
         super(WeightedCeLoss, self).__init__()
         self.pos_wt = torch.from_numpy(pos_weight).cuda()
         self.neg_wt = torch.from_numpy(neg_weight).cuda()
 
     def forward(self, prob, targets, infos, wt=None):
-        prob = prob.clamp(min=1e-7, max=1-1e-7)
+        prob = prob.clamp(min=1e-7, max=1-1e-7)                                                     #clamps it to ensure that the probability is valid
         if wt is None:
-            wt1 = torch.ones_like(prob)
+            wt1 = torch.ones_like(prob)                                                             #if there is no weight, then ones is assigned
         if config.TRAIN.CE_LOSS_WEIGHTED and self.pos_wt is not None:
-            wt1 = wt * (targets.detach() * self.pos_wt + (1-targets.detach()) * self.neg_wt)
+            wt1 = wt * (targets.detach() * self.pos_wt + (1-targets.detach()) * self.neg_wt)        #calculates weighted loss
 
         loss = -torch.mean(wt1 * (torch.log(prob) * targets + torch.log(1-prob) * (1-targets)))
 
@@ -42,9 +42,9 @@ class CeLossRhem(nn.Module):
         if wt is None:
             wt = torch.ones_like(prob)
         prob = prob.clamp(min=1e-7, max=1-1e-7)
-        with torch.no_grad():
+        with torch.no_grad():                                                                       #calculates the difference between probability and target
             prob_diff_wt = torch.abs((prob - targets) * wt) ** config.TRAIN.RHEM_POWER
-            idx = torch.multinomial(prob_diff_wt.view(-1), config.TRAIN.RHEM_BATCH_SIZE, replacement=True)
+            idx = torch.multinomial(prob_diff_wt.view(-1), config.TRAIN.RHEM_BATCH_SIZE, replacement=True)      #samples indeces based on weighted difference
             # hist = np.histogram(idx.cpu().numpy(), np.arange(torch.numel(prob)+1))[0]
             # hist = np.reshape(hist, prob.shape)
             # pos = np.where(hist == np.max(hist))
@@ -55,7 +55,18 @@ class CeLossRhem(nn.Module):
 
         targets = targets.view(-1)[idx]
         prob = prob.view(-1)[idx]
-        loss_per_smp = - (torch.log(prob) * targets + torch.log(1-prob) * (1-targets))
-        loss = loss_per_smp.mean()
+        loss_per_smp = - (torch.log(prob) * targets + torch.log(1-prob) * (1-targets))               #calculates loss per sample
+        loss = loss_per_smp.mean()                                                                   #calculates mean loss
 
         return loss
+
+#WeightedCeLoss is a loss function used for weighted binary cross entropy loss
+#During forward pass it computes the weighted CE between probabilities and targets
+#The loss is calculated by taking the negative mean of the weighted CE term
+
+#CeLossRhem is used for a RHEM cross-entropy (ranking with high error magnitude)
+#During the forward pass it computes the RHEM loss between predicted probabilities and targets
+#First it makes sure that the probabilities are within a valid range
+#Then it calculates a weighted difference between probabilities and targets using the weights
+#The function samples indices based on the weighted difference and selects the corresponding targets and probabilities 
+#The loss per sample is calculated as the negative log of the targets weighted by
