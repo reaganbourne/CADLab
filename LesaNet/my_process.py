9e970fa083630cd0bb0d794054cbb8fe72a4b56e
Reagan Bourne <reaganebourne@gmail.com>
2023-06-07 22:41:24 -0400

    I added a few comments to these files!

--- LesaNet/my_process.py
+++ LesaNet/my_process.py
@@ -17,16 +17,16 @@ from config import config, default
 from my_algorithm import select_triplets_multilabel
 
 
-def train(train_loader, model, criterions, optimizer, epoch):
-    batch_time = AverageMeter()
-    data_time = AverageMeter()
-    losses = AverageMeter()
-    accs = AverageMeter()
+def train(train_loader, model, criterions, optimizer, epoch):       #This function performs a training loop for one epoch 
+    batch_time = AverageMeter()                                     #tracks batch processing
+    data_time = AverageMeter()                                      #tracks data loading time
+    losses = AverageMeter()                                         #tracks losses
+    accs = AverageMeter()                                           #tracks accuracies
 
     # switch to train mode
     model.train()
 
-    end = time.time()
+    end = time.time()                                               #records the starting time of each epoch
     for i, (inputs, targets, infos) in enumerate(train_loader):
         # measure data loading time
         data_time.update(time.time() - end)
@@ -45,34 +45,34 @@ def train(train_loader, model, criterions, optimizer, epoch):
 
         # compute losses
         emb = out['emb']
-        A, P, N = select_triplets_multilabel(emb, target_clsf)
-        loss_metric = criterions['metric'](A, P, N)
+        A, P, N = select_triplets_multilabel(emb, target_clsf)                              #select triplets for the metric loss calculation
+        loss_metric = criterions['metric'](A, P, N)                                         #calculates the metric loss
 
-        prob1 = out['class_prob1']
-        loss_ce1 = criterions['wce'](prob1, target_clsf, infos, wt=target_clsf_wt)
-        loss_rhem = criterions['rhem'](prob1, target_clsf, infos, wt=rhem_wt)
+        prob1 = out['class_prob1']                                                          #gets the class_prob1 tensor from the out dictionary
+        loss_ce1 = criterions['wce'](prob1, target_clsf, infos, wt=target_clsf_wt)          #calculated the weighted CE loss for prob1
+        loss_rhem = criterions['rhem'](prob1, target_clsf, infos, wt=rhem_wt)               #calculates the RHEM for prob1
         if config.SCORE_PROPAGATION:
             prob2 = out['class_prob2']
-            loss_ce2 = criterions['wce'](prob2, target_clsf, infos, wt=target_clsf_wt)
+            loss_ce2 = criterions['wce'](prob2, target_clsf, infos, wt=target_clsf_wt)      #calculates the weighted CE for prob2
 
-            sub_losses = [loss_ce1, loss_rhem, loss_metric, loss_ce2]
+            sub_losses = [loss_ce1, loss_rhem, loss_metric, loss_ce2]                       #corresponds the sub losses and weight names
             wts_names = ['CE_LOSS_WT_1', 'RHEM_LOSS_WT', 'TRIPLET_LOSS_WT', 'CE_LOSS_WT_2']
         else:
-            sub_losses = [loss_ce1, loss_rhem, loss_metric]
+            sub_losses = [loss_ce1, loss_rhem, loss_metric]                                 #correspons the sub losses and weight names
             wts_names = ['CE_LOSS_WT_1', 'RHEM_LOSS_WT', 'TRIPLET_LOSS_WT']
 
         loss = 0
-        wts = [eval('config.TRAIN.' + name1) for name1 in wts_names]
+        wts = [eval('config.TRAIN.' + name1) for name1 in wts_names]                        #retrieves the weights based on the weight names
         for wt1, loss1 in zip(wts, sub_losses):
-            loss += wt1 * loss1
+            loss += wt1 * loss1                                                             #calculates the weighted sum of the sub losses
 
-        losses.update(loss.item())
+        losses.update(loss.item())                                                          #updates the losses with the current loss
 
         # compute gradient and do SGD step
-        optimizer.zero_grad()
-        loss.backward()
-        clip_gradient(model, default.clip_gradient)
-        optimizer.step()
+        optimizer.zero_grad()                                                               #clears the gradients
+        loss.backward()                                                                     #uses back propogation
+        clip_gradient(model, default.clip_gradient)                                         #clips the gradients to prevent for exploding
+        optimizer.step()                                                                    #updates the parameters
 
         # measure accuracy
         if config.SCORE_PROPAGATION:
@@ -83,9 +83,9 @@ def train(train_loader, model, criterions, optimizer, epoch):
         pred_labels = score2label(prob_np, config.TEST.SCORE_PARAM)
         targets_np = target_clsf.detach().cpu().numpy()
         target_unc = target_unc.numpy()
-        acc = compute_all_acc_wt(targets_np > 0, pred_labels, prob_np, target_unc == 0)[config.TEST.CRITERION]
+        acc = compute_all_acc_wt(targets_np > 0, pred_labels, prob_np, target_unc == 0)[config.TEST.CRITERION]   
 
-        accs.update(acc)
+        accs.update(acc)                                                                   #updates the accuraces with the current accuracy
 
         # measure elapsed time
         batch_time.update(time.time() - end)
@@ -105,42 +105,42 @@ def train(train_loader, model, criterions, optimizer, epoch):
 
 
 def validate(val_loader, model, use_val_th=False):
-    batch_time = AverageMeter()
+    batch_time = AverageMeter()                                                             #measures batch processing time
     # switch to evaluate mode
     model.eval()
 
-    with torch.no_grad():
-        end = time.time()
+    with torch.no_grad():                   #disables gradient descent
+        end = time.time()                   #record the start time of validation
         for i, (inputs, targets, infos) in enumerate(val_loader):
             if default.generate_features_all:
                 logger.info('generating features, batch %d', i)
-            filenames = [info[0] for info in infos]
-            lesion_idxs = [info[1] for info in infos]
-            inputs = [input.cuda() for input in inputs]
-            unc_targets = targets[1]
+            filenames = [info[0] for info in infos]                         #extracts filenames from info
+            lesion_idxs = [info[1] for info in infos]                       #extracts lesion indices from info
+            inputs = [input.cuda() for input in inputs]                     #moves inputs to the GPU
+            unc_targets = targets[1]                                        #extracts uncertain targets and actual targets
             targets = targets[0]
 
             # compute output
-            out = model(inputs)
+            out = model(inputs)                                             #does a forward pass through the model to get the output
             if config.SCORE_PROPAGATION:
-                prob_np = out['class_prob2'].detach().cpu().numpy()
+                prob_np = out['class_prob2'].detach().cpu().numpy()         
                 scores_np = out['class_score2'].detach().cpu().numpy()
             else:
                 prob_np = out['class_prob1'].detach().cpu().numpy()
                 scores_np = out['class_score1'].detach().cpu().numpy()
 
-            target1 = targets.numpy() > 0
-            pred_wt = unc_targets.numpy() == 0
+            target1 = targets.numpy() > 0                                       #converts targets to a numpy array, and checks if its >0
+            pred_wt = unc_targets.numpy() == 0                                  #converts uncertain targets to 0
             if i == 0:
-                target_all = target1
+                target_all = target1                                            #initialization for target, prob, score, lesion_idx, pred
                 prob_all = prob_np
-                score_all = scores_np
+                score_all = scores_np       
                 lesion_idx_all = lesion_idxs
                 pred_wt_all = pred_wt
                 if default.generate_features_all:
                     ft_all = out['emb']
             else:
-                target_all = np.vstack((target_all, target1))
+                target_all = np.vstack((target_all, target1))                   #vertically stacks all of the following
                 prob_all = np.vstack((prob_all, prob_np))
                 score_all = np.vstack((score_all, scores_np))
                 pred_wt_all = np.vstack((pred_wt_all, pred_wt))
@@ -159,7 +159,7 @@ def validate(val_loader, model, use_val_th=False):
             accs = compute_all_acc_wt(target_all, pred_label_all, prob_all, pred_wt_all)
 
         # measure elapsed time
-        batch_time.update(time.time() - end)
+        batch_time.update(time.time() - end)                    #updates the batch time
         end = time.time()
 
         if i % default.frequent == 0:
@@ -171,11 +171,10 @@ def validate(val_loader, model, use_val_th=False):
                    accs=accs[config.TEST.CRITERION]
             ))
 
-        print_accs(accs)
-        accs['ex_neg'] = np.sum((target_all == 0) & pred_wt_all, axis=0)
-
+        print_accs(accs)                                        #prints accuracy metrics
+        accs['ex_neg'] = np.sum((target_all == 0) & pred_wt_all, axis=0)           
         if use_val_th:  # only save for test set not val set
-            save_acc_to_file(accs, val_loader, 'all_terms')
+            save_acc_to_file(accs, val_loader, 'all_terms')                                         #saves accuracy and test scores to file
         if default.mode == 'infer' and use_val_th:
             save_test_scores_to_file(score_all, pred_label_all, target_all, accs, lesion_idx_all)
 
@@ -183,10 +182,25 @@ def validate(val_loader, model, use_val_th=False):
 
 
 def adjust_learning_rate(optimizer, epoch):
-    idx = np.where(epoch >= np.array([0]+default.lr_epoch))[0][-1]
-    lr_factor = default.lr_factor ** idx
-    for param_group in optimizer.param_groups:
-        if 'ori_lr' not in param_group.keys():  # first iteration
+    idx = np.where(epoch >= np.array([0]+default.lr_epoch))[0][-1]              #gets the index of learning rate epoch
+    lr_factor = default.lr_factor ** idx                                        #computes the learning rate factor
+    for param_group in optimizer.param_groups:                                  #iterates over the parameter groups in the optimizer
+        if 'ori_lr' not in param_group.keys():  # first iteration               #saves the original learning rate
             param_group['ori_lr'] = param_group['lr']
-        param_group['lr'] = param_group['ori_lr'] * lr_factor
-    logger.info('learning rate factor %g' % lr_factor)
+        param_group['lr'] = param_group['ori_lr'] * lr_factor                   #adjusts the learning rate
+    logger.info('learning rate factor %g' % lr_factor)                          #log the learning rate factor
+
+
+    #summary: 
+    #The train function performs a training loop for one epoch. It tracks batch processing time, data loading time, losses and accurarcies
+    #It then sets the model to train mode, constructs targets based on the input data, and runs the model forward to get output
+    #Then it computes different losses (metric loss, weighted CE, and RHEM)
+    #Then it calculates the overall loss and updates the average loss, it also computes the gradients to update the parameters
+    #Then it measures the accuracy by comparing the predicted labels to the actual output, and updates the accuracy
+
+    #The validate function performs validation on data given. It first processes the batch of inputs to make the output probabilities
+    #It calculates the predicted labels from the probabilities, and updates the overall targets, probabilities, scores, lesion indicces and weights
+    #Then it calculates the accuract, and saves the metrics and test scores
+
+    #The adjust learning rate function adjusts the learning rate of the optimizer based on the current epoch
+    #it calculates the learning rate factor based on the epoch number, and adjusts the learning rate for every parameter in the optimizer
--- LesaNet/network_vgg.py
+++ LesaNet/network_vgg.py
@@ -23,80 +23,80 @@ class VGG16bn(nn.Module):
     def __init__(self, num_cls, pretrained_weights=True):
         super(VGG16bn, self).__init__()
         self.num_cls = num_cls
-        self._make_conv_layers(batch_norm=True)
-        self._make_linear_layers(num_cls, roipool=5, fc=256, emb=config.EMBEDDING_DIM, norm=True)
+        self._make_conv_layers(batch_norm=True)                                                             #creates a convolutional layer with batch normalization
+        self._make_linear_layers(num_cls, roipool=5, fc=256, emb=config.EMBEDDING_DIM, norm=True)           #creates linear layers for classification and embedding
         if pretrained_weights:
-            self._load_pretrained_weights()
+            self._load_pretrained_weights()                                                                 #loads pretrained weights
         self._initialize_weights()
 
     def _load_pretrained_weights(self):
-        pretr = models.vgg16_bn(pretrained=True)
+        pretr = models.vgg16_bn(pretrained=True)                                                            #loads pretrained weights from VGG16 with batch normalization
         pretrained_dict = pretr.state_dict().items()
         pretrained_dict = {k: v for k, v in pretrained_dict}
         model_dict = self.state_dict()
-        pretr_keys = [0, 3, 7, 10, 14, 17, 20, 24, 27, 30, 34, 37, 40]
+        pretr_keys = [0, 3, 7, 10, 14, 17, 20, 24, 27, 30, 34, 37, 40]                                      #defines corresponding keys(layers) for loading the weights
         load_dict = {}
         for i in range(len(pretr_keys)):
-            load_dict[self.conv_layer_names[i]+'.weight'] = pretrained_dict['features.%d.weight' % pretr_keys[i]]
+            load_dict[self.conv_layer_names[i]+'.weight'] = pretrained_dict['features.%d.weight' % pretr_keys[i]]                   #extract and map weights to the corresponding layers
             load_dict[self.conv_layer_names[i]+'.bias'] = pretrained_dict['features.%d.bias' % pretr_keys[i]]
             load_dict[self.bn_layer_names[i]+'.weight'] = pretrained_dict['features.%d.weight' % (pretr_keys[i]+1)]
             load_dict[self.bn_layer_names[i]+'.bias'] = pretrained_dict['features.%d.bias' % (pretr_keys[i]+1)]
             load_dict[self.bn_layer_names[i]+'.running_mean'] = pretrained_dict['features.%d.running_mean' % (pretr_keys[i]+1)]
             load_dict[self.bn_layer_names[i]+'.running_var'] = pretrained_dict['features.%d.running_var' % (pretr_keys[i]+1)]
 
-        model_dict.update(load_dict)
+        model_dict.update(load_dict)                                                                                                #updates the dictionary with the new model data
 
     def _make_conv_layers(self, batch_norm=False):
-        cfg = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512]
-        block_idx = 1
+        cfg = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512]                                   #defines the configuration of the convolutional layers
+        block_idx = 1                                                                                                               #index for the blocks and layers
         layer_idx = 1
-        in_channels = config.NUM_SLICES
-        self.conv_layer_names = []
-        self.bn_layer_names = []
+        in_channels = config.NUM_SLICES                                                                                             #number of input channels
+        self.conv_layer_names = []                                                                                                  #creates lists to store the names of the convolutional layers
+        self.bn_layer_names = []                                                                                                    #creates lists to store batch normalization names
         for v in cfg:
-            if v == 'M':
+            if v == 'M':                                                                                                            #max pooling layer
                 layer = nn.MaxPool2d(kernel_size=2, stride=2)
                 name = 'pool%d' % block_idx
-                exec('self.%s = layer' % name)
-                block_idx += 1
+                exec('self.%s = layer' % name)                                                                                     
+                block_idx += 1                                                                                                      #increments the block and layer
                 layer_idx = 1
             else:
                 layer = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
                 name = 'conv%d_%d' % (block_idx, layer_idx)
-                self.conv_layer_names.append(name)
-                exec('self.%s = layer' % name)
+                self.conv_layer_names.append(name)                                                                                 #store the name of the convolutional layer
+                exec('self.%s = layer' % name)                                                                                     #create a na,ed attribute with the layer
                 if batch_norm:
-                    layer = nn.BatchNorm2d(v)
+                    layer = nn.BatchNorm2d(v)                                                                                      #batch normalization layer
                     name = 'bn%d_%d' % (block_idx, layer_idx)
                     exec('self.%s = layer' % name)
                     self.bn_layer_names.append(name)
-                layer = nn.ReLU(inplace=True)
-                name = 'relu%d_%d' % (block_idx, layer_idx)
+                layer = nn.ReLU(inplace=True)                                                                                      #reLU activation function
+                name = 'relu%d_%d' % (block_idx, layer_idx)                                                                        
                 exec('self.%s = layer' % name)
 
-                in_channels = v
-                layer_idx += 1
+                in_channels = v                                                                                                     #update the number of input channels for the next layer
+                layer_idx += 1                                                                                                      #move to the next layer
 
     def _make_linear_layers(self, num_cls, roipool=5, fc=512, emb=1024, norm=True):
         lin_in = roipool * roipool
-        self.roipool1 = _RoIPooling(roipool, roipool, 1)
+        self.roipool1 = _RoIPooling(roipool, roipool, 1)                    #first ROI pooling layer
         self.fc_pool1 = nn.Linear(lin_in * 64, fc)
 
-        self.roipool2 = _RoIPooling(roipool, roipool, .5)
+        self.roipool2 = _RoIPooling(roipool, roipool, .5)                   #second ROI pooling layer
         self.fc_pool2 = nn.Linear(lin_in * 128, fc)
 
-        self.roipool3 = _RoIPooling(roipool, roipool, .25)
+        self.roipool3 = _RoIPooling(roipool, roipool, .25)                  #third ROI pooling layer
         self.fc_pool3 = nn.Linear(lin_in * 256, fc)
 
-        self.roipool4 = _RoIPooling(roipool, roipool, .125)
+        self.roipool4 = _RoIPooling(roipool, roipool, .125)                 #fourth ROI pooling layer
         self.fc_pool4 = nn.Linear(lin_in * 512, fc)
 
-        self.roipool5 = _RoIPooling(roipool, roipool, .0625)
+        self.roipool5 = _RoIPooling(roipool, roipool, .0625)                #fifth ROI pooling layer
         self.fc_pool5 = nn.Linear(lin_in * 512, fc)
 
-        self.fc_emb = nn.Linear(fc*5, emb)
-        self.class_scores1 = nn.Linear(fc * 5, num_cls)
-        self.class_scores2 = nn.Linear(num_cls, num_cls)
+        self.fc_emb = nn.Linear(fc*5, emb)                                  #fully connected layer for embedding
+        self.class_scores1 = nn.Linear(fc * 5, num_cls)                     #fully connected layer for class scores
+        self.class_scores2 = nn.Linear(num_cls, num_cls)                    #fully connected layer for score propogation
 
     def forward(self, inputs):
         x, out_box, center_box = inputs
@@ -105,21 +105,21 @@ class VGG16bn(nn.Module):
         out_box = torch.cat((idxs, out_box), 1)
         center_box = torch.cat((idxs, center_box), 1)
 
-        x = self.relu1_1(self.bn1_1(self.conv1_1(x)))
+        x = self.relu1_1(self.bn1_1(self.conv1_1(x)))                       #forward pass through convolutional layers and pooling layers   
         x = self.relu1_2(self.bn1_2(self.conv1_2(x)))
         pool1 = self.pool1(x)
         roipool1 = self.roipool1(x, center_box)
         roipool1 = roipool1.view(bs, -1)
         fc_pool1 = self.fc_pool1(roipool1)
 
-        x = self.relu2_1(self.bn2_1(self.conv2_1(pool1)))
+        x = self.relu2_1(self.bn2_1(self.conv2_1(pool1)))                   #forward pass for the second set of layers
         x = self.relu2_2(self.bn2_2(self.conv2_2(x)))
         pool2 = self.pool2(x)
         roipool2 = self.roipool2(x, center_box)
         roipool2 = roipool2.view(bs, -1)
         fc_pool2 = self.fc_pool2(roipool2)
 
-        x = self.relu3_1(self.bn3_1(self.conv3_1(pool2)))
+        x = self.relu3_1(self.bn3_1(self.conv3_1(pool2)))                   #forward pass for the third set of layers
         x = self.relu3_2(self.bn3_2(self.conv3_2(x)))
         x = self.relu3_3(self.bn3_3(self.conv3_3(x)))
         pool3 = self.pool3(x)
@@ -127,7 +127,7 @@ class VGG16bn(nn.Module):
         roipool3 = roipool3.view(bs, -1)
         fc_pool3 = self.fc_pool3(roipool3)
 
-        x = self.relu4_1(self.bn4_1(self.conv4_1(pool3)))
+        x = self.relu4_1(self.bn4_1(self.conv4_1(pool3)))                   #forward pass for the fourth set of layers
         x = self.relu4_2(self.bn4_2(self.conv4_2(x)))
         x = self.relu4_3(self.bn4_3(self.conv4_3(x)))
         pool4 = self.pool4(x)
@@ -135,34 +135,42 @@ class VGG16bn(nn.Module):
         roipool4 = roipool4.view(bs, -1)
         fc_pool4 = self.fc_pool4(roipool4)
 
-        x = self.relu5_1(self.bn5_1(self.conv5_1(pool4)))
+        x = self.relu5_1(self.bn5_1(self.conv5_1(pool4)))                   #forward pass for the fifth set of layers
         x = self.relu5_2(self.bn5_2(self.conv5_2(x)))
         x = self.relu5_3(self.bn5_3(self.conv5_3(x)))
         roipool5 = self.roipool5(x, out_box)
         roipool5 = roipool5.view(bs, -1)
         fc_pool5 = self.fc_pool5(roipool5)
 
-        fc_cat = torch.cat((fc_pool1, fc_pool2, fc_pool3, fc_pool4, fc_pool5), dim=1)
-        emb = self.fc_emb(fc_cat)
-        emb = F.normalize(emb, p=2, dim=1)
+        fc_cat = torch.cat((fc_pool1, fc_pool2, fc_pool3, fc_pool4, fc_pool5), dim=1)           #concatenated the 5 pooling layers
+        emb = self.fc_emb(fc_cat)                                                               #passes the concatenated features through a fully connnected layer for embedding
+        emb = F.normalize(emb, p=2, dim=1)                                                      #normalizes the embedding vectors
 
         out = {}
-        out['emb'] = emb
+        out['emb'] = emb                                                                        #stores the normalized embedding vectors in the output dictionary
 
-        class_score1 = self.class_scores1(fc_cat)
-        class_prob1 = torch.sigmoid(class_score1)
-        out['class_score1'] = class_score1
+        class_score1 = self.class_scores1(fc_cat)                                               #passes concatenated features through a fully connected layer to obtain class scores
+        class_prob1 = torch.sigmoid(class_score1)                                               #applies sigmoid activation function to the class scores to obtain class probabilities
+        out['class_score1'] = class_score1                                                      #stores the class scores and probabilities in the dictionary
         out['class_prob1'] = class_prob1
         if config.SCORE_PROPAGATION:
-            class_score2 = self.class_scores2(class_score1)
-            class_prob2 = torch.sigmoid(class_score2)
-            out['class_score2'] = class_score2
+            class_score2 = self.class_scores2(class_score1)                                     #passes the class scores through another fully connected layer to obtain class scores
+            class_prob2 = torch.sigmoid(class_score2)                                           #applies sigmoid activation function to the propogated class scores to obtain class probabilities
+            out['class_score2'] = class_score2                                                  #stores propogates class scores and probabilities to the dictionary
             out['class_prob2'] = class_prob2
 
         return out
 
-    def _initialize_weights(self):
+    def _initialize_weights(self):          
         for m in self.modules():
             if isinstance(m, nn.Linear) and m.weight.shape == (self.num_cls, self.num_cls):
-                m.weight.data[:] = torch.eye(self.num_cls)
-                m.bias.data.zero_()
+                m.weight.data[:] = torch.eye(self.num_cls)                                      #initializes the weight matrix of specific linear layers
+                m.bias.data.zero_()                                                             #initializes the bias vector to zere
+
+#summary:
+#The network is first initialized and the weights are loaded and put with the corresponding layers of the network
+#the make_conv_layers function defines the convolutional layers with batch normalization
+#The make_linear_layers function defines the ROI pooling layers and the corresponding fully connected layers for feature extraction
+#It also defines the fully connected layer for embedding and for class scores
+#The forward function does the forward pass of the network, computes the embedding vectors and class scores, applies activatin functions, and stores the results in the output dictionary
+#The initialize_weights function initializes the weights of linear layers
